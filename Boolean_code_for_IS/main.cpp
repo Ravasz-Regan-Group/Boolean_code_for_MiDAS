383c1a6ae16246a3ed6024ebbdfd914d8cea190d
Erzsébet Regan <erzsoravasz@gmail.com>
2016-09-06 14:29:01 -0400

    Initial Commit

---
+++ Boolean_code_for_IS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Boolean_code_for_IS
+//
+//  Created by Erzsó Ravasz Regan on 9/6/16.
+//  Copyright © 2016 Regan_Group. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
